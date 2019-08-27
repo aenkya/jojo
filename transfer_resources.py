bb7767f2e25912456a2a9aaf224a387b21602cad
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2019-08-27 16:45:37 +0300

    Download resources

--- transfer_resources.py
+++ transfer_resources.py
@@ -1,7 +1,18 @@
+import os
 from boto3 import s3
+import botocore
+
+BUCKET_NAME = os.getenv('BUCKET_NAME') or ''
+KEY = os.getenv('KEY') or ''
 
 def transfer_resources():
-    pass
+    try:
+        s3.Bucket(BUCKET_NAME).download_file(KEY, '')
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] =='404':
+            print('The object does not exist')
+        else:
+            raise
 
 if __name__ == '__main__':
     transfer_resources()
